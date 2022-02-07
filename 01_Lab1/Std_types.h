b46347859f49d7c55c50bc699e85394ea72a7003
Michael Ibrahim <michael.ibrahim042@gmail.com>
2022-02-07 18:27:38 +0200

    Add lab1 tasks

---
+++ 01_Lab1/Std_types.h
@@ -0,0 +1,22 @@
+/* **************************************************************************************
+**       Author      :	Michael S. Ibrahim
+**       Date        :	February 07, 2022
+**       Version     :	V01
+**       SWC         :	Std_types
+**       Description :	Definition of standard data types
+** **************************************************************************************/
+#ifndef _STD_TYPES_H
+#define _STD_TYPES_H
+
+  typedef unsigned char             u8;
+  typedef signed   char             s8;
+  typedef unsigned short int        u16;
+  typedef signed   short int        s16;
+  typedef unsigned long int         u32;
+  typedef signed   long int         s32;
+  typedef unsigned long long int    u64;
+  typedef signed long long int      s64;
+  typedef float                     f32;
+  typedef double                    f64;
+  
+#endif
